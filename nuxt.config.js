a47a166aad7d6a701820a72c73efdc92cafe3f3c
komura <affirman03139@outlook.jp>
2020-10-19 22:09:54 +0900

    feat(contentful): contentfulと接続

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,34 @@
 import colors from 'vuetify/es5/util/colors'
 
+const pkg = require('./package')
+const { getConfigForKeys } = require('./lib/config.js')
+const ctfConfig = getConfigForKeys([
+  'CTF_BLOG_POST_TYPE_ID',
+  'CTF_SPACE_ID',
+  'CTF_CDA_ACCESS_TOKEN',
+])
+
+const { createClient } = require('./plugins/contentful')
+const cdaClient = createClient(ctfConfig)
+
+module.exports = {
+  // 省略、、、
+  generate: {
+    routes() {
+      return cdaClient
+        .getEntries(ctfConfig.CTF_BLOG_POST_TYPE_ID)
+        .then((entries) => {
+          return [...entries.items.map((entry) => `/blog/${entry.fields.slug}`)]
+        })
+    },
+  },
+  env: {
+    CTF_SPACE_ID: ctfConfig.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: ctfConfig.CTF_CDA_ACCESS_TOKEN,
+    CTF_BLOG_POST_TYPE_ID: ctfConfig.CTF_BLOG_POST_TYPE_ID,
+  },
+}
+
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
